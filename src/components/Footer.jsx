cf480597698b02b68479ff41b2d8f0100816e0f8
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-09 20:43:48 +0600

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -79,6 +79,27 @@ const Footer = () => {
                         </Link>
                     </nav>
                 </div>
+                <div className=" w-full md:w-1/4">
+                    <h1 className=" font-medium text-xl pb-4 pt-5 md:pt-0">Contact Us</h1>
+                    <nav className=" flex flex-col gap-2">
+                        <Link to="/" spy={true} smooth={true} duration={500}>
+                            123 Elm Street, Suite 456 Springfield, IL 62701 United States
+                        </Link>
+                        <Link to="/" spy={true} smooth={true} duration={500}>
+                            support@care.com
+                        </Link>
+                        <Link to="/" spy={true} smooth={true} duration={500}>
+                            +123-456-7890
+                        </Link>
+                    </nav>
+                </div>
+            </div>
+            <div>
+                <p className=" text-center py-4">
+                    @copyright developed by
+                    <span className=" text-hoverColor"> champion programmers</span> | All
+                    rights reserved
+                </p>
             </div>
         </div>
     );
