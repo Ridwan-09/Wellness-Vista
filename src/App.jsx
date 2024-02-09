d5a9c2ec1cf0a153d9ae50a2b2c4661028918c95
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-09 17:21:24 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
+
+      <main>
+        <div id="home">
+          <Home></Home>
+        </div>
+      </main>
+
     </div>
   );
 };
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from "../layouts/Button";
+
+const Home = () => {
+    return (
+        <div className=" w-full lg:w-4/5 space-y-5 mt-10">
+            <h1 className="text-5xl font-bold leading-tight">Empowering Health Choices for a Vibrant Life Your Trusted..</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam magnam omnis natus accusantium quos. Reprehenderit incidunt expedita molestiae impedit at sequi dolorem iste sit culpa, optio voluptates fugiat vero consequatur?</p>
+
+        </div>
+    );
+};
+
+export default Home;
