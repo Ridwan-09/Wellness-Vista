d912ab066cca28ab96190684b0cb089218f60837
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-09 19:34:07 +0600

    first commit

---
+++ src/components/Doctors.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+const Doctors = () => {
+    return (
+        <div>
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Doctors;
