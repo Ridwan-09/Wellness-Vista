39dd1f90e83b0853b3887caa5dac464090ab1c15
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-09 19:42:37 +0600

    first commit

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -91,6 +91,45 @@ const Doctors = () => {
                         quidem.
                     </p>
                 </div>
+
+                <div className="flex gap-5 mt-4 lg:mt-0">
+                    <button
+                        className=" bg-[#d5f2ec] text-backgroundColor px-4 py-2 rounded-lg active:bg-[#ade9dc]"
+                        onClick={() => slider.current.slickPrev()}
+                    >
+                        <FaArrowLeft size={25} />
+                    </button>
+                    <button
+                        className=" bg-[#d5f2ec] text-backgroundColor px-4 py-2 rounded-lg active:bg-[#ade9dc]"
+                        onClick={() => slider.current.slickNext()}
+                    >
+                        <FaArrowRight size={25} />
+                    </button>
+                </div>
+            </div>
+
+            <div className=" mt-5">
+                <Slider ref={slider} {...settings}>
+                    {data.map((e, index) => (
+                        <div
+                            className="h-[350px] text-black rounded-xl shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] mb-2 cursor-pointer"
+                            key={index}
+                        >
+                            <div>
+                                <img
+                                    src={e.img}
+                                    alt="img"
+                                    className=" h-56 rounded-t-xl w-full"
+                                />
+                            </div>
+
+                            <div className=" flex flex-col justify-center items-center">
+                                <h1 className=" font-semibold text-xl pt-4">{e.name}</h1>
+                                <h3 className=" pt-2">{e.specialties}</h3>
+                            </div>
+                        </div>
+                    ))}
+                </Slider>
             </div>
         </div>
     );
