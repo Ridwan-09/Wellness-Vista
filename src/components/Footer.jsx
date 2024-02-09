cda36d89df58646ac0791ef2762cedd230767a62
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-09 20:26:32 +0600

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,14 +6,17 @@ const Footer = () => {
         <div className=" bg-backgroundColor text-white rounded-t-3xl mt-8 md:mt-0">
             <div className="flex flex-col md:flex-row justify-between p-8 md:px-32 px-5">
                 <div className=" w-full md:w-1/4">
-                    <h1>
+                    <h1 className=" font-semibold text-xl pb-4">
                         WellnessVista
-                        </h1>
-                    <p>
-                    Our team of dedicated doctors, each specializing in unique fields
-            such as orthopedics, cardiology, pediatrics, neurology, dermatology,
-            and more.
+                    </h1>
+                    <p className=" text-sm">
+                        Our team of dedicated doctors, each specializing in unique fields
+                        such as orthopedics, cardiology, pediatrics, neurology, dermatology,
+                        and more.
                     </p>
+                </div>
+                <div>
+                    
                 </div>
             </div>
         </div>
