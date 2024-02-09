aad2c228381b69e1d0a12803b0a907ca71747661
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-09 16:28:20 +0600

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from 'react-scroll';
+import Button from "../layouts/Button";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import Contact from "../models/Contact";
 
@@ -29,6 +30,11 @@ const Navbar = () => {
             <div>
                 <div className=" flex flex-row justify-between p-5 md:px-32 px-5 bg-backgroundColor shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
                     <div className=" flex flex-row items-center cursor-pointer">
+                        <Link to="home" spy={true} smooth={true} duration={500}>
+                            <h1 className=" text-2xl font-semibold">WellnessVista.</h1>
+                        </Link>
+                    </div>
+                    <div className=" hidden lg:flex flex-row items-center text-lg font-medium gap-8">
                         <Link
                             to="home"
                             spy={true}
@@ -110,58 +116,58 @@ const Navbar = () => {
                     className={`${menu ? "translate-x-0" : "-translate-x-full"
                         } lg:hidden flex flex-col absolute bg-backgroundColor text-white left-0 top-16 font-semibold text-2xl text-center pt-8 pb-4 gap-8 w-full h-fit transition-transform duration-300`}
                 >
-                <Link
-                    to="home"
-                    spy={true}
-                    smooth={true}
-                    duration={500}
-                    className=" hover:text-hoverColor transition-all cursor-pointer"
-                    onClick={closeMenu}
-                >
-                    Home
-                </Link>
-                <Link
-                    to="about"
-                    spy={true}
-                    smooth={true}
-                    duration={500}
-                    className=" hover:text-hoverColor transition-all cursor-pointer"
-                    onClick={closeMenu}
-                >
-                    About Us
-                </Link>
-                <Link
-                    to="services"
-                    spy={true}
-                    smooth={true}
-                    duration={500}
-                    className=" hover:text-hoverColor transition-all cursor-pointer"
-                    onClick={closeMenu}
-                >
-                    Services
-                </Link>
-                <Link
-                    to="doctors"
-                    spy={true}
-                    smooth={true}
-                    duration={500}
-                    className=" hover:text-hoverColor transition-all cursor-pointer"
-                    onClick={closeMenu}
-                >
-                    Doctors
-                </Link>
-                <Link
-                    to="blog"
-                    spy={true}
-                    smooth={true}
-                    duration={500}
-                    className=" hover:text-hoverColor transition-all cursor-pointer"
-                    onClick={closeMenu}
-                >
-                    Blog
-                </Link>
+                    <Link
+                        to="home"
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        className=" hover:text-hoverColor transition-all cursor-pointer"
+                        onClick={closeMenu}
+                    >
+                        Home
+                    </Link>
+                    <Link
+                        to="about"
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        className=" hover:text-hoverColor transition-all cursor-pointer"
+                        onClick={closeMenu}
+                    >
+                        About Us
+                    </Link>
+                    <Link
+                        to="services"
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        className=" hover:text-hoverColor transition-all cursor-pointer"
+                        onClick={closeMenu}
+                    >
+                        Services
+                    </Link>
+                    <Link
+                        to="doctors"
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        className=" hover:text-hoverColor transition-all cursor-pointer"
+                        onClick={closeMenu}
+                    >
+                        Doctors
+                    </Link>
+                    <Link
+                        to="blog"
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        className=" hover:text-hoverColor transition-all cursor-pointer"
+                        onClick={closeMenu}
+                    >
+                        Blog
+                    </Link>
                 </div>
-                
+
                 <div>
 
                     <div className=" lg:hidden">
