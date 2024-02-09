27ae74d258061f156c239843bd39ef9b8612c8f9
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-09 18:42:19 +0600

    first commit

---
+++ src/components/Services.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    <h1>Our Services</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione exercitationem nam ullam beatae recusandae.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
