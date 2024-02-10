f8eb84a493cf490b704cb6fbed8dd94d6adee022
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-10 11:56:43 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Services from './components/Services';
-import Doctors from './components/Doctors';
 import Blogs from './components/Blogs';
 import Footer from './components/Footer';
+import Doctors from './components/Doctors';
 
 const App = () => {
   return (
